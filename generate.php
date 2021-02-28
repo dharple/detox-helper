7a550661ab918918da5b06da8c6ff3341bfd0b3a
Doug Harple <dharple@gmail.com>
2021-02-28 00:19:29 -0500

    Stop generating single characters

--- generate.php
+++ generate.php
@@ -59,39 +59,3 @@ foreach ($codes as $code) {
     printf("\")\n");
     printf("export UTF8_%04X;\n\n", $code);
 }
-
-//foreach ($codes as $code) {
-//    printf("echo \$UTF8_%04X;\n", $code);
-//}
-
-foreach ($codes as $code) {
-    if ($code > 0x100) {
-        continue;
-    }
-
-    $char = chr($code);
-    printf("# Hex: ", $char);
-    for ($i = 0; $i < strlen($char); $i++) {
-        printf("\\x%02x", ord($char[$i]));
-    }
-    foreach ($lines as $line) {
-        $contents = explode(';', $line);
-        $id = intval($contents[0], 16);
-        if ($id !== $code) {
-            continue;
-        }
-
-        $isControl = ($contents[1] == '<control>');
-        $label = $contents[1];
-        $altLabel = $contents[10];
-        printf(" / %s", (!$isControl ? $label : $altLabel) ?: 'undefined');
-
-        break;
-    }
-    printf("\nCHAR_%02X=\$(printf \"", $code);
-    for ($i = 0; $i < strlen($char); $i++) {
-        printf("\\%03o", ord($char[$i]));
-    }
-    printf("\")\n");
-    printf("export CHAR_%02X;\n\n", $code);
-}
